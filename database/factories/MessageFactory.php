6a3860f7f4a124afaa5d8aab176804a1e7ab9a53
maxim_tszyu <tszyuoa@gmail.com>
2025-04-09 15:45:07 +0500

    filled out migration, wrote seeder, factory and configured database seeder

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use App\Models\Message;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
  */
@@ -14,10 +14,13 @@ class MessageFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    protected $model = Message::class;
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'message' => $this->faker->sentence
         ];
     }
 }
